d019b9cb11ea2786106d9ce0ec89156080c2596d
Zachary Voase <zacharyvoase@me.com>
2010-05-09 05:28:06 +0000

    Added an XML namespace helper.

---
+++ lib/xmlns.rb
@@ -0,0 +1,15 @@
+XMLNS = {
+  "rdf" => "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+  "rdfs" => "http://www.w3.org/2000/01/rdf-schema#",
+  "dc" => "http://purl.org/dc/elements/1.1/",
+  "foaf" => "http://xmlns.com/foaf/0.1/",
+  "xsd" => "http://www.w3.org/2001/XMLSchema",
+}
+
+def xmlns
+  namespaces = {}
+  XMLNS.each do |key, value|
+    namespaces["xmlns:#{key}"] = value
+  end
+  namespaces
+end
